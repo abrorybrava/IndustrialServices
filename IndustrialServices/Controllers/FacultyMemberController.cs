0fc5825cefcc1578124be669548c347eeb53874c
abrorybrava <abrorybravaaa@gmail.com>
2024-01-03 18:31:51 +0700

    photo aman es

--- IndustrialServices/Controllers/FacultyMemberController.cs
+++ IndustrialServices/Controllers/FacultyMemberController.cs
@@ -4,6 +4,12 @@ namespace IndustrialServices.Controllers
 {
     public class FacultyMemberController : Controller
     {
+        private readonly IWebHostEnvironment _webhost;
+        public FacultyMemberController (IWebHostEnvironment webhost)
+        {
+            _webhost = webhost;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +23,33 @@ namespace IndustrialServices.Controllers
         {
             ViewBag.Id = id;
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadPhoto(IFormFile fotopengajar)
+        {
+            var imgext = Path.GetExtension(fotopengajar.FileName);
+            var saveimg = Path.Combine(_webhost.WebRootPath, "assets", "img", fotopengajar.FileName);
+
+            if (imgext == ".jpg" || imgext == ".png" || imgext == ".jpeg")
+            {
+                // Check if the file with the same name exists in the directory
+                if (!System.IO.File.Exists(saveimg))
+                {
+                    using (var uploading = new FileStream(saveimg, FileMode.Create))
+                    {
+                        await fotopengajar.CopyToAsync(uploading);
+                        return Ok("Data berhasil diupload ke wwwroot");
+                    }
+                }
+                else
+                {
+                    return Ok("File dengan nama yang sama sudah ada di direktori");
+                }
+            }
+            else
+            {
+                return Ok("Format foto tidak valid. Harap unggah file dengan format .jpg atau .png");
+            }
         }
     }
 }
