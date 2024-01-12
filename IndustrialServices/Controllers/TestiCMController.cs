1e1a52de461ef4c7b1e5ff85438d7a9b352431da
abrorybrava <abrorybravaaa@gmail.com>
2024-01-12 09:12:00 +0700

    tarik

---
+++ IndustrialServices/Controllers/TestiCMController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IndustrialServices.Controllers
+{
+    public class TestiCMController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IndustrialServices/Controllers/TipeFasilitasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IndustrialServices.Controllers
+{
+    public class TipeFasilitasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
